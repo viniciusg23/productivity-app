cdea1baaec52063fbef6fa77c7c880b46d0e303a
viniciusg23 <viniciusgonzagaguilherme@gmail.com>
2024-03-16 16:54:32 -0300

    code:routes configuration setup

---
+++ app/(tabs)/metrics.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+function MetricsScreen(){
+    return(
+        <Text>Metrics Screen</Text>
+    );
+}
+
+
+export default MetricsScreen;
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+function SettingsScreen(){
+    return(
+        <Text>Settings Screen</Text>
+    );
+}
+
+
+export default SettingsScreen;
